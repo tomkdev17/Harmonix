a474f4edb34b9f9d1c61f82cba65ee3c0e57fb95
tomkdev17 <143559210+tomkdev17@users.noreply.github.com>
2024-11-12 16:36:51 -0600

    Add angular dev port to CORS

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const passport = require('passport');
 require('./passport');
 
 
-let allowedOrigins = [ 'https://harmonix-daebd0a88259.herokuapp.com/', 'http://localhost:8080', 'http://localhost:1234', 'https://harmonix-db.netlify.app'];
+let allowedOrigins = [ 'https://harmonix-daebd0a88259.herokuapp.com/', 'http://localhost:8080', 'http://localhost:1234', 'https://harmonix-db.netlify.app', 'http://localhost:4200'];
 
 app.use(cors({
     origin: (origin, callback) => {
